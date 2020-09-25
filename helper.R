6427cfc932f4821eaec7fb1708084c9224962635
Juan Carlos Villasenor Derbez <juancarlos@ucsb.edu>
2020-09-25 12:11:48 -0700

    Deleting gz and adding csvs

--- helper.R
+++ helper.R
@@ -1,12 +1,14 @@
 
+# Delete state data
+file.remove(file.path("lex_data", list.files("lex_data", pattern = "state")))
 
-
+# Identify gz files
 cmd <- paste0("gunzip lex_data/", list.files("lex_data", pattern = ".csv.gz"))
 
+# Extract gz files
 purrr::walk(cmd, system)
 
+# Delete remaining gz files
 file.remove(file.path("lex_data", list.files("lex_data", pattern = ".csv.gz")))
-
-file.remove(file.path("lex_data", list.files("lex_data", pattern = "state")))
 
 
